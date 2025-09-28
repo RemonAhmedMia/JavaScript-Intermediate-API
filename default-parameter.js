d5a9d468fe9d21793d2377ce7a6bb3cb414e1f1e
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-28 20:30:49 +0100

    dflt-prms

--- default-parameter.js
+++ default-parameter.js
@@ -4,3 +4,12 @@ function add(num1, num2) {
     console.log(result);
 }
 add(3,4); //argument
+
+
+// Default Parameter========
+function sum ( num1=0, num2=0){
+    const final = num1 + num2;
+    console.log(final);
+    
+}
+sum(100,10)
