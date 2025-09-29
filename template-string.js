a25b74131c1da7d49e2f7ce2c7f71c667675ca94
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-29 18:38:35 +0100

    template-two

--- template-string.js
+++ template-string.js
@@ -19,4 +19,29 @@ This is line three
 console.log(multiLine);
 
 
-const friendOne = "Omar Faruk"
+const friendOne = "Omar Faruk";
+const friendTwo = "Moin Intejar";
+const friendThree = "Someone";
+const places = "Kaptai And Noakhali"
+const tour = `we have three friend like ${friendOne} and ${friendTwo} also ${friendThree} we are going the place of ${places}`
+
+console.log(tour);
+
+// এখানে ${name} হলো placeholder।
+// Backtick ব্যবহার করলে আমরা string-এর মধ্যে variable / expression বসাতে পারি।
+
+// Expression বসানো
+const a = 10;
+const b = 20;
+
+console.log(`Sum is: ${a + b}`); 
+// Output: Sum is: 30
+
+
+// Function এর ভেতরে====
+function greet(name) {
+  return `Welcome, ${name}!`;
+}
+
+console.log(greet("Mia"));
+// Output: Welcome, Mia!
