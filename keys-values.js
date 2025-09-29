b564c0dbe10e8dc3a76cce922c1e646d667c2a3f
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-29 22:06:40 +0100

    object-seal

--- keys-values.js
+++ keys-values.js
@@ -4,5 +4,9 @@ const manosh = {
     country: 'Bangladesh'
 }
 
+Object.seal(manosh); // tahole ar object er vitore noton kore kichu add kora jabe na like manosh.hobby = "gerdening" kora jabe na; kinto value change kora jabe
+
+manosh.age = 20;
+
 console.log(Object.keys(manosh));
 console.log(Object.values(manosh));
