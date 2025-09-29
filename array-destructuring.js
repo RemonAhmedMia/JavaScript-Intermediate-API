e94c2926c3335094d52931e4ce2b345590a0f736
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-29 21:34:28 +0100

    array-destructuring

--- array-destructuring.js
+++ array-destructuring.js
@@ -1,13 +1,31 @@
-const sania = {
-    name: 'Sania Ahmed',
-    age : 180,
-    friends: ['Anika', 'Maimona' , 'Shakiba'],
-    status: 'Not-Found'
-};
+// const sania = {
+//     name: 'Sania Ahmed',
+//     age : 180,
+//     friends: ['Anika', 'Maimona' , 'Shakiba'],
+//     status: 'Not-Found'
+// };
 
-//console.log(sania.name);
-//console.log(sania.age);
-//evahbe bar bar likhe kora jhamela
+// //console.log(sania.name);
+// //console.log(sania.age);
+// //evahbe bar bar likhe kora jhamela
 
-const {name, age, friends, status} = sania;
-console.log(name);
+// const {name, age, friends, status} = sania;
+// console.log(name);
+// এখানে { name, age, friends } হলো object এর keys যেগুলো আমরা variable হিসেবে declare করেছি।
+
+// array destructring==============
+
+// const person = ['rimon' , {age: 10} , 'shakib'];
+// const [nayok,boyos,villain] = person
+
+// console.log(nayok);
+
+
+const person = ['Rimon', { age: 10, city: 'Dhaka' }, 'Shakib', 'Sakib2'];
+
+const [nayok, { age: boyos, city }, ...villains] = person;
+
+console.log(nayok);    // Rimon
+console.log(boyos);    // 10
+console.log(city);     // Dhaka
+console.log(villains); // ['Shakib', 'Sakib2']
