64ac79bc5b3b7e3db701baf0fa44325ace0ef3b2
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-29 21:15:36 +0100

    object-destructuring

---
+++ array-destructuring.js
@@ -0,0 +1,13 @@
+const sania = {
+    name: 'Sania Ahmed',
+    age : 180,
+    friends: ['Anika', 'Maimona' , 'Shakiba'],
+    status: 'Not-Found'
+};
+
+//console.log(sania.name);
+//console.log(sania.age);
+//evahbe bar bar likhe kora jhamela
+
+const {name, age, friends, status} = sania;
+console.log(name);
