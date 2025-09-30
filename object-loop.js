7218d68be318703a2af67c58b67fd3bc30137500
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-30 10:36:48 +0100

    for-of

--- object-loop.js
+++ object-loop.js
@@ -4,7 +4,19 @@ const team = {
     country: 'Bangladesh'
 }
 
-for ( let key in team) {
-    //console.log(key); key gula pawa jabe just
-    console.log(`key: ${key} value: ${team[key]}`);
+// for ( let key in team) {
+//     //console.log(key); key gula pawa jabe just
+//     console.log(`key: ${key} value: ${team[key]}`);
+// }
+
+ // ekhon amra chaile for of diyeo korte pari sekhetre object k array te convert korte hobe Object.entreies diye|
+
+//  for(let key of Object.entries(team)) {
+//     console.log(key); // evhabe object k array te nile 3 ta array dhekabe
+//  }
+
+//ekhn amra object k destructring korte pari
+
+for(let [key,value] of Object.entries(team)) {
+    console.log(`The key Is: ${key} And value is: ${value}`);
 }
